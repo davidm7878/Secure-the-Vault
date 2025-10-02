2c84fb04b38ea873afd74a717e562a8e19663442
David M <davidm7878@gmail.com>
2025-10-01 21:12:53 -0500

    completed Secure the Vault

---
+++ index.js
@@ -0,0 +1,17 @@
+let code1;
+
+code1 = 7 + 3;
+
+let code2 = 84 / 2 - 2;
+
+let code3 = 100 % 61;
+
+let message = "The vault has been secured. The combination is:";
+
+let codeA = code1 + "-" + code2 + "-" + code3;
+
+let codeB = `${code1}-${code2}-${code3}`;
+
+console.log(message, codeA, codeB);
+
+
